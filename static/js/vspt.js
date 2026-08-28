53e39c52adc17edd015467d2ac233f26a2808356
Adrian Fish <adrian.r.fish@gmail.com>
2026-08-28 23:45:06 +0100

    Added cdn path

--- static/js/vspt.js
+++ static/js/vspt.js
@@ -17,11 +17,11 @@ $.get(`/content/vspt/${dialang.session.al}/${dialang.session.tl}.html`, function
 
         // Switch the button image appropriately.
         if ($(this).hasClass('valid-button')) {
-            $('#' + this.id + '_image').attr('src', '/images/trueselected.gif');
-            $('#' + wordId + '_incorrect_image').attr('src', '/images/false.gif');
+            $('#' + this.id + '_image').attr('src', 'https://pub-c5c4a068a24045f68b85a172153bd668.r2.dev/images/trueselected.gif');
+            $('#' + wordId + '_incorrect_image').attr('src', 'https://pub-c5c4a068a24045f68b85a172153bd668.r2.dev/images/false.gif');
         } else {
-            $('#' + this.id + '_image').attr('src', '/images/falseselected.gif');
-            $('#' + wordId + '_correct_image').attr('src', '/images/true.gif');
+            $('#' + this.id + '_image').attr('src', 'https://pub-c5c4a068a24045f68b85a172153bd668.r2.dev/images/falseselected.gif');
+            $('#' + wordId + '_correct_image').attr('src', 'https://pub-c5c4a068a24045f68b85a172153bd668.r2.dev/images/true.gif');
         }
 
         // Have we answered all the words?
