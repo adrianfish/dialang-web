d7e7460f9b6e869c7c002e9b91079743e26169cb
Adrian Fish <adrian.r.fish@gmail.com>
2026-09-15 18:19:48 +0100

    Updated report index file

--- src/templates/reports.ts
+++ src/templates/reports.ts
@@ -7,6 +7,7 @@ export const reports = `
     <div><a href="/reports/sessions">Session Report</a></div>
     <div><a href="/reports/al-distribution">Distribution by Administration Language</a></div>
     <div><a href="/reports/tl-distribution">Distribution by Test Language</a></div>
+    <div><a href="/reports/skill-distribution">Distribution by Skill</a></div>
   </body>
 </html>
 `;
